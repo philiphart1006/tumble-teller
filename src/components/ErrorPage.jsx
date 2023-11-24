8b893a4a289325b6ad29b0aae50ba643fa992e8d
philiphart1006 <philiphart1006@gmail.com>
2023-11-24 11:33:35 +0000

    adds tailored error page for failing to fetch error

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,21 @@
 import { useRouteError } from "react-router-dom";
+import Otto from "../assets/otto.jpg"
 
 export default function ErrorPage() {
   const error = useRouteError()
+  console.log('ERROR: ',error)
 
-  return (
+  return (error.message === "Failed to fetch"
+  ?
+  (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Failed to fetch data - please go back and enter just the first half of your postcode</p>
+      <img className='error-image' src={Otto}></img>
+    </div>
+  )
+  :
+  (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
@@ -11,5 +23,6 @@ export default function ErrorPage() {
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
+  )
   )
 }
